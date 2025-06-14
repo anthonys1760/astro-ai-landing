807f04a3e9c5841edc2759f3a5620ba19cd879a2
Anthony Smith <anthony.smith1760@gmail.com>
2025-06-14 09:04:23 -0400

    Configure for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://anthonys1760.github.io',
+  base: '/astro-ai-landing',
+  output: 'static'
 });
