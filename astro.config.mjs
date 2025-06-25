13f8af61dba6c6e95d24360e39cb06abd3ff5b58
Anthony Smith <anthony.smith1760@gmail.com>
2025-06-25 00:02:09 -0400

    Got the custom domain working 100%. No blockers

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,11 +9,9 @@ export default defineConfig({
     applyBaseStyles: true,
   })],
   site: 'https://anthonys1760.github.io',
-  base: '/astro-ai-landing',
   output: 'static',
   build: {
-    assets: '_astro',
-    assetsPrefix: '/astro-ai-landing'
+    assets: '_astro'
   },
   vite: {
     build: {
